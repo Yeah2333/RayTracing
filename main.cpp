fbb0f05cb8773e56a4b14733e5a5de35c7559235
Yeah2333 <653091941@qq.com>
2020-11-29 15:25:44 +0800

    finish multithread with ugly code ,need simple code.

--- main.cpp
+++ main.cpp
@@ -11,55 +11,94 @@
 #include <iostream>
 #include <material.h>
 
+#include <vector>
+#include <thread>
+
 //
 hittable_list random_scene();
 color ray_color(const ray& r, const hittable& world, int depth) ;
+void raytracing_thread(int k);
+
+const auto aspect_ratio = 3.0 / 2.0;  //image width/height
+const int image_width = 1200;
+const int image_height = static_cast<int>(image_width / aspect_ratio);
+const int samples_per_pixel = 500;
+const int max_depth = 50;
+
+// World
+
+auto world = random_scene();
+
+// Camera
+point3 lookfrom(13,2,3);
+point3 lookat(0,0,0);
+vec3 vup(0,1,0);
+auto dist_to_focus = 10.0;
+auto aperture = 0.1;
+
+camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+
+int thread_num = 48;
 
 int main() {
-    // Image
-    const auto aspect_ratio = 3.0 / 2.0;  //image width/height
-    const int image_width = 1200;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 500;
-    const int max_depth = 50;
-
-    // World
-
-   auto world = random_scene();
-
-    // Camera
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
-    vec3 vup(0,1,0);
-    auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
-
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+//    // Image
+//    const auto aspect_ratio = 3.0 / 2.0;  //image width/height
+//    const int image_width = 1200;
+//    const int image_height = static_cast<int>(image_width / aspect_ratio);
+//    const int samples_per_pixel = 500;
+//    const int max_depth = 50;
+//
+//    // World
+//
+//   auto world = random_scene();
+//
+//    // Camera
+//    point3 lookfrom(13,2,3);
+//    point3 lookat(0,0,0);
+//    vec3 vup(0,1,0);
+//    auto dist_to_focus = 10.0;
+//    auto aperture = 0.1;
+//
+//    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     // Render
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            color pixel_color(0, 0, 0);
 
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
-                ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
-            }
 
-            write_color(std::cout, pixel_color, samples_per_pixel);
-        }
+    std::vector<std::thread> threads;
+    for (int k = 0; k < thread_num ; ++k) {
+        threads.push_back(std::thread(raytracing_thread,k));
+    }
+    for (auto &thread : threads) {
+        thread.join();
 
     }
+
+
+
+//    for (int j = image_height - 1; j >= 0; --j) {
+//        std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
+//        for (int i = 0; i < image_width; ++i) {
+//            color pixel_color(0, 0, 0);
+//
+//            for (int s = 0; s < samples_per_pixel; ++s) {
+//                auto u = (i + random_double()) / (image_width-1);
+//                auto v = (j + random_double()) / (image_height-1);
+//                ray r = cam.get_ray(u, v);
+//                pixel_color += ray_color(r, world, max_depth);
+//            }
+//
+//            write_color(std::cout, pixel_color, samples_per_pixel);
+//        }
+//
+//    }
     std::cerr << "\nDone.\n";
 }
 
 color ray_color(const ray& r, const hittable& world, int depth) {
+
     hit_record rec;
 
     // If we've exceeded the ray bounce limit, no more light is gathered.
@@ -136,5 +175,25 @@ hittable_list random_scene() {
     return world;
 
 
+}
+
+void raytracing_thread(int k ){
+
+    for (int j = image_height - k; j >= 0; j-=thread_num){
+        std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            color pixel_color(0, 0, 0);
+
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world, max_depth);
+            }
+
+            write_color(std::cout, pixel_color, samples_per_pixel);
+        }
+
+    }
 }
 
